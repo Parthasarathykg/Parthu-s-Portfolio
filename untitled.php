593079c4688f5e547f5145db5a85e14682c8ce24
Parthasarathy K G <parthasarathy.gopinath@astrazeneca.com>
2021-07-21 02:23:07 +0530

    email

--- untitled.php
+++ untitled.php
@@ -1,4 +1,5 @@
 <?php
+phpinfo();
 error_reporting(E_ALL);
 $to      = 'parthukg1@gmail.com'; //your email
 $subject = 'from my website  - email form'; 
@@ -6,6 +7,7 @@ $message = $_POST['message'];
 $headers = 'From: '.$_POST['email']. "\r\n".
 'Reply-To: myemail@mail.com' . "\r\n".
 'X-Mailer: PHP/'.phpversion(); 
+echo "<h2>" . $to . $subject . "</h2>";
 
 mail($to, $subject, $message, $headers); 
 ?>  
