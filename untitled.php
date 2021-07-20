53bdfde36446db6c837d01f23fe6f4a5d96fd406
Parthasarathy K G <parthasarathy.gopinath@astrazeneca.com>
2021-07-21 02:16:25 +0530

    php bug

--- untitled.php
+++ untitled.php
@@ -1,10 +1,11 @@
 <?php
-    $to      = 'parthukg1@gmail.com'; //your email
-    $subject = 'from my website  - email form'; 
-    $message = $_POST['message'];
-    $headers = 'From: '.$_POST['email']. "\r\n".
-        'Reply-To: myemail@mail.com' . "\r\n".
-        'X-Mailer: PHP/'.phpversion(); 
+error_reporting(E_ALL);
+$to      = 'parthukg1@gmail.com'; //your email
+$subject = 'from my website  - email form'; 
+$message = $_POST['message'];
+$headers = 'From: '.$_POST['email']. "\r\n".
+'Reply-To: myemail@mail.com' . "\r\n".
+'X-Mailer: PHP/'.phpversion(); 
 
 mail($to, $subject, $message, $headers); 
 ?>  
