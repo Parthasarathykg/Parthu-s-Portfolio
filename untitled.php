b4b34b72c2717c8511fb265b3f2ef1b943608dbf
Parthasarathy K G <parthasarathy.gopinath@astrazeneca.com>
2021-07-21 03:10:23 +0530

    heroku email commit

--- untitled.php
+++ untitled.php
@@ -1,12 +1,44 @@
 <?php
-error_reporting(E_ALL);
-$to      = 'parthukg1@mailinator.com'; //your email
-$subject = 'from my website  - email form'; 
-$message = $_POST['message'];
-$headers = 'From: '.$_POST['email']. "\r\n".
-'Reply-To: myemail@mail.com' . "\r\n".
-'X-Mailer: PHP/'.phpversion(); 
-echo "<h2>" . $to . $subject . $message . $headers . "</h2>";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-mail($to, $subject, $message, $headers); 
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+//Enable SMTP debugging.
+$mail->SMTPDebug = 3;                               
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();            
+//Set SMTP host name                          
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                          
+//Provide username and password     
+$mail->Username = "name@gmail.com";                 
+$mail->Password = "super_secret_password";                           
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                           
+//Set TCP port to connect to
+$mail->Port = 587;                                   
+
+$mail->From = "name@gmail.com";
+$mail->FromName = "Full Name";
+
+$mail->addAddress("name@example.com", "Recepient Name");
+
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+try {
+    $mail->send();
+    echo "Message has been sent successfully";
+} catch (Exception $e) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
 ?>  
