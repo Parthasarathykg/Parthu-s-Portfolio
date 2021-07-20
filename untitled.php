7f4d6f4f72583c1081070ba6d24533ade0616ff2
Parthasarathy K G <parthasarathy.gopinath@astrazeneca.com>
2021-07-21 03:39:15 +0530

    email

--- untitled.php
+++ untitled.php
@@ -6,6 +6,45 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
+
+//$to_mail = "architects@palavin.com,t.lavin@palavin.com,12yorkcourt@gmail.com";
+$to_mail = "parthukg1@gmail.com";
+//$cc="paul@enhance.ie";
+$mail_sent = 0;
+if(isset($_POST['submit'])){
+    //echo "the form was submitted";
+
+$name = trim(strip_tags($_POST['name']));
+if($name == "")
+    $error = true;
+
+$email = trim(strip_tags($_POST['email']));
+if($email == "")
+    $error = true;
+
+if($error != true){
+    $headers = 'From: ' .$name . $email ."\r\n";
+    //$headers .= 'CC: "'.$cc.'" <'.$cc.'>'."\r\n";
+    $headers .= 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "";
+
+    $subject = "New contact message";
+
+    $message = "New Contact message, received from: <br /> \n ";
+    $message .= "<b>Name</b> ".$name."<br /> \n";
+    $message .= "<b>Email</b> ".$email."<br /> \n";
+
+    if(@mail($to_mail,$subject,$message,$headers))
+    {
+        echo "mail sent";
+        $mail_sent = 1;
+    }
+    else echo "mail not sent";
+} else {
+    echo 'validation error';
+}
+}
+
 $mail = new PHPMailer(true);
 
 //Enable SMTP debugging.
@@ -27,13 +66,13 @@ $mail->Port = 587;
 $mail->From = "test@gmail.com";
 $mail->FromName = "Partha";
 
-$mail->addAddress("partha@mailinator.com", "Maillinator");
+$mail->addAddress($email, $name);
 
 $mail->isHTML(true);
 
-$mail->Subject = "Subject Text";
-$mail->Body = "<i>Mail body in HTML</i>";
-$mail->AltBody = "This is the plain text version of the email content";
+$mail->Subject = "Thank you from Parthu";
+$mail->Body = "<i>Thank you for taking your time and writing in! I Will try to get in touch with you soon.</i>";
+$mail->AltBody = "Thank you for taking your time and writing in! I Will try to get in touch with you soon.";
 
 try {
     $mail->send();
