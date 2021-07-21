859a6f006ccd872316ab30528501ec8d2d1cd0a0
Parthasarathy K G <parthasarathy.gopinath@astrazeneca.com>
2021-07-21 05:42:00 +0530

    email

--- untitled.php
+++ untitled.php
@@ -23,7 +23,7 @@ if($email == "")
     $error = true;
 
 if($error != true){
-    $headers = 'From: ' .$name . $email ."\r\n";
+    $headers = 'From: ' . $email ."\r\n";
     //$headers .= 'CC: "'.$cc.'" <'.$cc.'>'."\r\n";
     $headers .= 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=utf-8' . "";
